b0fb2a380f21e3f8510e87b5c95543a333dbfa96
KSoundarrajan <soundarrajan.kaliaperumal@gmail.com>
2018-03-23 10:28:59 +0100

    algorithms

--- range_for.cpp
+++ range_for.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -31,5 +33,29 @@ map <int, int> keyValue ({{1,1}, {2,2}, {3,3}, {4,4}, {5,5}});
 for (auto i : keyValue) {
 	cout << i.first << " " << i.second << endl;
 }
+
+vector<int> result;
+map<int, int> key_value ({{1,2}, {3,4}, {5,6}, {7,8}, {9,10}});
+for (auto i : key_value) {
+	result.push_back(pow(i.first, distance(key_value.find(i.first), key_value.end())) + i.second);
+	cout << i.first << distance(key_value.find(i.first), key_value.end()) << endl;
+
+}
+
+int ar[6] = {1,2,3,4,5,-6};
+all_of(ar, ar+6, [] (int x) {return x > 0;}) ? cout << "all are positive" << endl : cout << "all are not positive" << endl;
+
+any_of(ar, ar+6, [] (int x) {return x < 0;}) ? cout << "all are not positive" << endl : cout << "all are positive" << endl;
+
+none_of(ar, ar+6, [] (int x) {return x == 0;}) ? cout << "none are zeros" << endl : cout << "there exists a zero" << endl;
+
+
+int copy_vec[6];
+copy_n(ar+2, 4, copy_vec);
+for(auto i : copy_vec) {
+	cout << i << endl;
+}
+
+
 
 }
